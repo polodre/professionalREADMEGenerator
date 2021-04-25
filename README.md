695bcc2f2bce9642efa49567a83fdffb500fd8be
Andre Lucas <andre.lucas284@gmail.com>
2021-04-24 22:32:58 -0400

    commiting code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# professionalREADMEGenerator
+# 
+![Badge here](https://img.shields.io/badge/license-MIT-blue)
+## Description - 
+## Install
+you will install dependencies with the command : npm install
--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-// TODO: Include packages needed for this application
+// when prompted, generate README
+// include Title, Description, ToC, installation, usage, license, contributing, tests, questions
 
+// license will have choices
+// if license add badge
+
+// questions need github username, link, email
+
+// ToC needs link to section in README
+
+
+// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+const fs = require('fs');
+var generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {    
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Success!')
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'title',
+            },
+            {
+              type: 'input',
+              message: 'What is your description?',
+              name: 'description',
+            },
+            {
+              type: 'input',
+              message: 'How would you install your dependencies?',
+                name: 'install',
+              default: 'npm install',
+            },
+            {
+              type: 'input',
+              message: 'How would you use this application?',
+                name: 'usage',
+            },
+            {
+              type: 'list',
+              message: 'Choose a license',
+                name: 'license',
+                choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+            },
+            {
+              type: 'input',
+              message: 'Who can contribute to the application?',
+                name: 'contributers',
+            },
+            {
+              type: 'input',
+              message: 'How would you test the application?',
+                name: 'tests',
+                default: 'npm test'
+            },
+            {
+              type: 'input',
+              message: 'Who would you contact if you have questions?',
+                name: 'questions',
+                
+            }
+            
+        ])
+        .then((response) => {
+            //console.log(response);
+            writeToFile("README.md", generateMarkdown(response))
+        })
+}
 
 // Function call to initialize app
 init();
